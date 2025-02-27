fee89156b002770b719eeb4cefe2f04ff6bc55ce
boyuia <dodylan190@gmail.com>
2025-02-28 07:01:15 +1100

    Update UIRender.cs

--- rendering/UIRender.cs
+++ rendering/UIRender.cs
@@ -7,3 +7,14 @@ void RenderUI(VkCommandBuffer commandBuffer, VkPipeline pipeline, VkBuffer verte
 
     VulkanNative.vkCmdDraw(commandBuffer, 4, 1, 0, 0);
 }
+
+void RenderUIBatch(VkCommandBuffer commandBuffer, VkPipeline pipeline, VkBuffer vertexBuffer, int quadCount)
+{
+    VulkanNative.vkCmdBindPipeline(commandBuffer, VkPipelineBindPoint.Graphics, pipeline);
+    
+    VkDeviceSize offset = 0;
+    VulkanNative.vkCmdBindVertexBuffers(commandBuffer, 0, 1, ref vertexBuffer, ref offset);
+
+    // Draw all UI quads in one call
+    VulkanNative.vkCmdDraw(commandBuffer, quadCount * 4, 1, 0, 0);
+}
