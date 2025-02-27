cf4575319d7944dd2c0d83ba0c621712386efa6c
boyuia <dodylan190@gmail.com>
2025-02-28 07:00:55 +1100

    Update UIBinder.cs

--- rendering/UIBinder.cs
+++ rendering/UIBinder.cs
@@ -2,7 +2,26 @@ struct UIQuadVertex
 {
     public Vector2 position;
     public Vector2 texCoord;
+    public Vector4 color;  // Allow per-element colors
 }
+
+void CreateUIVertexBuffer(VkDevice device)
+{
+    // Reserve space for multiple UI elements (e.g., 1000 quads)
+    int maxQuads = 1000;
+    int vertexCount = maxQuads * 4;
+
+    VkBufferCreateInfo bufferInfo = new VkBufferCreateInfo
+    {
+        sType = VkStructureType.BufferCreateInfo,
+        size = (ulong)(vertexCount * Marshal.SizeOf(typeof(UIQuadVertex))),
+        usage = VkBufferUsageFlags.VertexBuffer,
+        sharingMode = VkSharingMode.Exclusive
+    };
+
+    VulkanNative.vkCreateBuffer(device, ref bufferInfo, IntPtr.Zero, out vertexBuffer);
+}
+
 
 VkBuffer vertexBuffer;
 VkDeviceMemory vertexMemory;
